0a9a9ccc0481f134f58ca50751f4ce0af6b63fd8
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-05-04 17:00:47 +0200

    almost ath the point

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,36 +6,65 @@ var ingredients = [
     ["lettuce", "1"],
     ["mustard", "1.5"],
     ["tomato", "1.5"],
-]
+];
+createInputs(ingredients, ".ingredients")
 
 //Genero gli input dei vari ingredienti
 
 function createInputs(ingredientsList, ingredientElement) {
     contatore = 0;
-    while (contatore < ingredientsList.lenght) {
-        document.querySelector(ingredientElement).insertAdjacentHTML("beforeend",
+    while (contatore < ingredientsList.length) {
+        document
+            .querySelector(ingredientElement)
+            .insertAdjacentHTML("beforeend",
         `
         <div class="form_group">
-            <img src="./assets/img/${ingredientsList[contatore][0] + '.svg'}">
-            <label for="${ingredientsList[contatore][0]}">${ingredientsList[contatore][0]}</label>
-            <input type="checkbox" name="${ingredientsList[contatore][0]}" id="${ingredientsList[contatore][0]}" data-price="${list[counter][1]}">
+        <img width = 30px; src="./assets/img/${ingredientsList[contatore][0] + '.svg'}">
+        <label for="${ingredientsList[contatore][0]}">${ingredientsList[contatore][0]}</label>
+        <input type="checkbox" name="${ingredientsList[contatore][0]}" id="${ingredientsList[contatore][0]}" data-price="${ingredientsList[contatore][1]}">
         </div>
         `  
         )
-        counter++
+        contatore++
     }
 }
-createInputs(ingredients, ".ingredients")
 
-/* for (var i = 0; i < ingredientsList.lenght; i++){
-    document.querySelector(ingredientsElement[i]);
-    ingredientElement.insertAdjacentHTML("beforeend",
-    `
-    <div class="form_group">
-        <img src="./assets/img/${ingredientsList[i][0] + '.svg'}">
-        <label for="${ingredientsList[i][0]}">${ingredientsList[i][0]}</label>
-        <input type="checkbox" name="${ingredientsList[i][0]}" id="${ingredientsList[i][0]}" data-price="${list[counter][1]}">
-    </div>
-    ` 
-    )
-} */
+//Definisco un prezzo di partenza del panino
+
+var fixedPrice = 8.99;
+
+//Calculate button
+//creo funzione per l'ascolto
+document.getElementById("calc_butt").addEventListener("click", function () {
+    //verifico quali checkbox sono stati selezionati
+    var checkChosen = document.querySelectorAll("input[type='checkbox']")
+    console.log(checkChosen);
+    //stabilisco il prezzo dei soli ingredienti
+    var totIngredients = null;
+    for (var i = 0; i < checkChosen.length; i++){
+        var chosenElement = checkChosen[i];
+        if (chosenElement.checked) {
+            totIngredients += Number(chosenElement.getAttribute('data-price'))
+        }
+    }
+    console.log(totIngredients);
+    //stabilisco il prezzo totale dei panini
+    var totalPriceWithoutDiscounts = totIngredients + fixedPrice;
+    console.log(totalPriceWithoutDiscounts);
+    //creo un array con una lista di discounts
+
+    var discountList = ["darthvader", "maythe4th", "iamyourfather", "deathstar"]
+    
+    var insertedCoupon = document.getElementById("coupon").value
+    console.log(insertedCoupon);
+    if (discountList == insertedCoupon) {
+        console.log((totalPriceWithoutDiscounts *25)/100);
+    } else {
+        console.log(totalPriceWithoutDiscounts);
+    }
+    
+})
+/* 
+console.log(totalPriceWithDiscounts); */
+
+
